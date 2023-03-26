b0fdf0b36b85151872bdb528d2acd9157cd25744
BuildTools <unconfigured@null.spigotmc.org>
2023-03-26 16:40:22 +0200

    Added controller

--- index.php
+++ index.php
@@ -5,34 +5,7 @@ declare(strict_types=1);
 namespace App;
 
 include_once('./src/utils/debug.php');
-include_once('./src/View.php');
+include_once('./src/Controller.php');
 
-const DEFAULT_ACTION = 'list';
-
-$action = $_GET['action'] ?? DEFAULT_ACTION;
-
-$viewParams = [];
-
-switch($action):
-    case 'create':
-        $page = 'create';
-        $created = false;
-
-        if(!empty($_POST)){
-            $viewParams = [
-                'title' => $_POST['title'],
-                'description' => $_POST['description'],
-            ];
-            $created = true;
-        }
-
-        $viewParams['created'] = $created;
-        break;
-
-    default:
-        $page = 'list';
-        $viewParams['resultList'] = 'Wyświetlamy listę notatek';
-    endswitch;
-
-$view = new View();
-$view->render($page, $viewParams);
+$controller = new Controller($_GET, $_POST);
+$controller->run();
