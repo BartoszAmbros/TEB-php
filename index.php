f1e38f47f53f52be7031da3a83862bdd54a7214c
BuildTools <unconfigured@null.spigotmc.org>
2023-03-26 14:15:18 +0200

    Code optimization

--- index.php
+++ index.php
@@ -7,7 +7,17 @@ namespace App;
 include_once('./src/utils/debug.php');
 include_once('./src/View.php');
 
-$action = $_GET['action'] ?? null;
+const DEFAULT_ACTION = 'list';
+
+$action = $_GET['action'] ?? DEFAULT_ACTION;
+
+$viewParams = [];
+
+if ($action === 'create'){
+    $viewParams['resultCreate'] = 'Tworzymy nową notatkę';
+}else {
+    $viewParams['resultList'] = 'Wyświetlamy listę notatek';
+}
 
 $view = new View();
-$view->render($action);
+$view->render($action, $viewParams);
