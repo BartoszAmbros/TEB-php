0d6af3f616650139ee61507a4bde71762fa032ef
BuildTools <unconfigured@null.spigotmc.org>
2023-04-16 16:24:40 +0200

    Added App Exception

--- index.php
+++ index.php
@@ -4,11 +4,18 @@ declare(strict_types=1);
 
 namespace App;
 
+require_once('./Exception/AppException.php');
 include_once('./src/utils/debug.php');
 include_once('./src/Controller.php');
 require_once('./config/config.php');
 
-Controller::initConfiguration($configuration);
+try{
+    Controller::initConfiguration($configuration);
 
-$controller = new Controller($_GET, $_POST);
-$controller->run();
+    $controller = new Controller($_GET, $_POST);
+    $controller->run();
+} catch(AppException $e) {
+    echo '<h1>Wystąpił błąd w aplikacji</h1>';
+    echo '<h3>' . $e->getMessage() . '</h3>';
+}
+
--- src/Database.php
+++ src/Database.php
@@ -4,11 +4,17 @@ declare(strict_types = 1);
 
 namespace App;
 
+use Exception;
 use PDO;
+use PDOException;
 
 class Database {
     public function __construct(array $config){
-        $dsn = "mysql:dbname={$config['database']};host={$config['host']}";
+        try{
+            $dsn = "mysql:dbname={$config['database']};host={$config['host']}";
         $connection = new PDO('ssssss');
+    }catch(PDOException $e) {
+        throw new AppException('Błąd w połączeniu z bazą danych');
+    }
     }
 }
