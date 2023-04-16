706fb735604d9377bc1bbb6b77e98f4d64c71d72
BuildTools <unconfigured@null.spigotmc.org>
2023-04-16 15:52:32 +0200

    Basic database connection added

--- index.php
+++ index.php
@@ -6,6 +6,9 @@ namespace App;
 
 include_once('./src/utils/debug.php');
 include_once('./src/Controller.php');
+require_once('./config/config.php');
+
+Controller::initConfiguration($configuration);
 
 $controller = new Controller($_GET, $_POST);
 $controller->run();
---
+++ src/Database.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App;
+
+use PDO;
+
+class Database {
+    public function __construct(array $config){
+        $dsn = "mysql:dbname={$config['database']};host={$config['host']}";
+        $connection = new PDO('ssssss');
+    }
+}
