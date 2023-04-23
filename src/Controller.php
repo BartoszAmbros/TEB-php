9340a6283d5d7b7babfd2a07b6a94f873e767cdd
BuildTools <unconfigured@null.spigotmc.org>
2023-04-23 13:01:42 +0200

    Code fixes

--- src/Controller.php
+++ src/Controller.php
@@ -36,25 +36,25 @@ class Controller {
         switch($this->action()):
         case 'create':
             $page = 'create';
-            $created = false;
+            
             $data = $this->getRequestPost();
 
             if(!empty($data)){
                 $created = true;
-                $viewParams = [
+                $noteData = [
                     'title' => $data['title'],
                     'description' => $data['description'],
                     ];
-                $this->database->createNote($viewParams);
-                header('Location: /');
+                $this->database->createNote($noteData);
+                header('Location: /?before=created');
             }
 
-        $viewParams['created'] = $created;
         break;
 
         default:
             $page = 'list';
-            $viewParams['resultList'] = 'Wyświetlamy listę notatek';
+            $data = $this->getRequestGet();
+            $viewParams['before'] = $data['before'] ?? null;
         endswitch;
 
         $this->view->render($page, $viewParams);
