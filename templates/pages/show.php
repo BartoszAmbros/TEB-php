cc4c7d5198402f6d30f3328d22ffc29c385c2a7f
BuildTools <unconfigured@null.spigotmc.org>
2023-05-06 13:28:20 +0200

    Controller refactoring

--- templates/pages/show.php
+++ templates/pages/show.php
@@ -5,7 +5,7 @@
             <li>ID: <?php echo (int) $note['id'] ?></li>
             <li>Tytuł: <?php echo $note['title'] ?></li>
             <li>Opis: <?php echo $note['description'] ?></li>
-            <li>Utworzono: <?php echo (int) $note['created'] ?></li>
+            <li>Utworzono: <?php echo $note['created'] ?></li>
             <li>
                 <a href="/">
                     <button>Powrót do listy notatek</button>
